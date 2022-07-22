3e71255ac18dd3571ca568780cb6d98e55112e9d
MarioAlva <mario.antonio.alva@estudiantat.upc.edu>
2022-07-22 19:30:37 +0200

    Version 0.1

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import logo_curriculum from '../img/Logo-Curriculum.png';
+import lang_esp from '../img/lang_esp.png';
+import lang_cat from '../img/lang_cat.png';
+import lang_usa from '../img/lang_usa.png';
+import lang_jap from '../img/lang_jap.png';
+import '../css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className='header-container'>
+          <img src={logo_curriculum} className="logo-curriculum" alt="LogoCurriculum"/>
+          <div className='select-language'>
+
+          </div>
+          <div className='hide'>
+            <img src={lang_esp} alt="spanish_flag"/>
+            <img src={lang_cat} alt="catalan_flag"/>
+            <img src={lang_usa} alt="usa_flag"/>
+            <img src={lang_jap} alt="japanese_flag"/>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
