ff1d4073df2023d785e366c14dd9f4bbcbcc8d52
MarioAlva <mario.antonio.alva@estudiantat.upc.edu>
2022-07-22 20:24:01 +0200

    Re-version 0.1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,8 @@ import lang_esp from '../img/lang_esp.png';
 import lang_cat from '../img/lang_cat.png';
 import lang_usa from '../img/lang_usa.png';
 import lang_jap from '../img/lang_jap.png';
+import arrow_down from '../img/arrow_down.png';
+import arrow_down_idle from '../img/arrow_down_idle.png';
 import '../css/App.css';
 
 function App() {
@@ -12,7 +14,8 @@ function App() {
         <div className='header-container'>
           <img src={logo_curriculum} className="logo-curriculum" alt="LogoCurriculum"/>
           <div className='select-language'>
-
+            <img className='flag-language' src={lang_esp} alt="spanish_flag"/>
+            <img className='arrow-down-language' src={arrow_down_idle} alt="arrow"/>
           </div>
           <div className='hide'>
             <img src={lang_esp} alt="spanish_flag"/>
