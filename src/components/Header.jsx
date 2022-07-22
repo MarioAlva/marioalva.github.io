7e4fad26f29a1ecbb751b2c5422a31aa577467fb
MarioAlva <mario.antonio.alva@estudiantat.upc.edu>
2022-07-22 22:48:14 +0200

    Version 0.2h

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import {useState} from 'react';
 import logo_curriculum from '../img/Logo-Curriculum.png';
 import lang_esp from '../img/lang_esp.png';
 import lang_cat from '../img/lang_cat.png';
@@ -8,14 +9,23 @@ import arrow_down_idle from '../img/arrow_down_idle.png';
 import '../css/App.css';
 
 function App() {
+  const [html] = document.getElementsByTagName("html")
+  const lang = html.getAttribute("lang");
+  const [mainLang, setMainLang] = useState(false);
+  const mainLangHover = event => {
+    // 👇️ toggle isActive state on click
+    setMainLang(current => !current);
+  };
   return (
     <div className="App">
       <header className="App-header">
         <div className='header-container'>
           <img src={logo_curriculum} className="logo-curriculum" alt="LogoCurriculum"/>
-          <div className='select-language'>
-            <img className='flag-language' src={lang_esp} alt="spanish_flag"/>
-            <img className='arrow-down-language' src={arrow_down_idle} alt="arrow"/>
+          <div id="select_lang" className={mainLang ? 'select-language-hover' : 'select-language'}
+            onMouseEnter={mainLangHover}
+            onMouseLeave={mainLangHover}>
+            <img className='flag-language' src={changeFlag()} alt="spanish_flag"/>
+            <img className='arrow-down-language' src={mainLang ? arrow_down : arrow_down_idle} alt="arrow"/>
           </div>
           <div className='hide'>
             <img src={lang_esp} alt="spanish_flag"/>
@@ -27,6 +37,19 @@ function App() {
       </header>
     </div>
   );
+  function changeFlag(){
+    switch(lang){
+      case "es": return lang_esp;
+      break;
+      case "cat": return lang_cat;
+      break;
+      case "jp": return lang_jap;
+      break;
+      default: return lang_usa
+    }
+  }
 }
+
+
 
 export default App;
